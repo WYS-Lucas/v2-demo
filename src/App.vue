0b753f91ea4761ec0d7bc73e248612d674b72261
IGLucas <weiyibo@bilibili.com>
2025-01-16 17:32:16 +0800

    TodoItem demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vue 2 TodoList</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a new todo" />
+    <ul>
+      <TodoItem
+        v-for="(todo, index) in todos"
+        :key="index"
+        :todo="todo"
+        @remove="removeTodo(todo)"
+        @update:completed="updateCompleted(index, $event)"
+      />
+    </ul>
+    <p v-if="todos.length === 0">No more todos!</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from "./components/TodoItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TodoItem,
+  },
+  data() {
+    return {
+      newTodo: "",
+      todos:[]
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === "") return;
+      this.todos.push({ text: this.newTodo.trim(), completed: false})
+      this.newTodo = "";
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    updateCompleted(index, completed) {
+      this.$set(this.todos[index], 'completed', completed)
+    }
+  },
+  created() {
+    const savedTodos = JSON.parse(localStorage.getItem("todos"));
+    if (savedTodos) {
+      this.todos = savedTodos;
+    }
+  },
+  watch: {
+    todos: {
+      handler(newTodos) {
+        localStorage.setItem("todos", JSON.stringify(newTodos))
+      },
+      deep: true // watch change inside the todos array
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+input {
+  padding: 10px;
+  width: 300px;
+  font-size: 16px;
+}
+ul {
+  list-style: none;
+  padding: 0;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <li :class="{ completed: todo.completed }">
+    <input type="checkbox" :checked="todo.completed" @change="toggleCompleted">
+    <span>{{ todo.text }}</span>
+    <button @click="$emit('remove')"> Delete </button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    toggleCompleted(event) {
+      this.$emit('update:completed', event.target.checked);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.completed span {
+  text-decoration: line-through;
+  color: gray;
+}
+button {
+  margin-left: 10px;
+}
+</style>
