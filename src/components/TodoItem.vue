7e5c6f75e73b4050fb9f8d9219b3ae62b1979713
IGLucas <weiyibo@bilibili.com>
2025-01-16 17:58:48 +0800

    注释

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,6 +1,6 @@
 <template>
   <li :class="{ completed: todo.completed }">
-    <input type="checkbox" :checked="todo.completed" @change="toggleCompleted">
+    <input type="checkbox" :checked="todo.completed" @change="toggleCompleted"> <!-- @是监听事件的缩写-->  <!-- :checked 是v-bind 指令的缩写-->
     <span>{{ todo.text }}</span>
     <button @click="$emit('remove')"> Delete </button>
   </li>
@@ -17,7 +17,7 @@ export default {
   },
   methods: {
     toggleCompleted(event) {
-      this.$emit('update:completed', event.target.checked);
+      this.$emit('update:completed', event.target.checked);  // $emit 将子组件中监听的事件传递给父组件，父组件再更新数据，而不是直接更新props
     }
   }
 }
